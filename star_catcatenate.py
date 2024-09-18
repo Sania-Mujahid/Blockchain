d28209bc04bac4f6681d04414b73ac10e6430578
Sania-Mujahid <dewaan51214@gmail.com>
2024-09-18 18:23:24 +0500

    Add files via upload
    CATCATENATION logic and implementation in python.

---
+++ star_catcatenate.py
@@ -0,0 +1,11 @@
+print("*****\n****\n***\n**\n*\n")
+print("*\n**\n***\n****\n*****")
+str1='sania'
+str2='dewaan'
+str3=str1+""+ str2
+print("your final string is",str3)
+print("your string length is\n")
+print(len(str3))
+str="sania_dewaan"
+#chr=str[0]
+print(str[11])
