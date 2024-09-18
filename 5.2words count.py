1b0058eb770f30bfc55c44682cca91b05a963c7d
Sania-Mujahid <dewaan51214@gmail.com>
2024-09-18 18:19:45 +0500

    Add files via upload
    words count for given string.

---
+++ 5.2words count.py
@@ -0,0 +1,8 @@
+#task2
+str=input("enter any string please\n")
+#counts the spaces in string
+spaces=str.count('')
+#the no of words is apporoximately the no of spaces plus 1
+word_count=spaces+1
+#the output
+print("the no of words in string are\n ",word_count)
